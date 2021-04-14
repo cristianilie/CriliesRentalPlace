06e0d0fe0182cdad218b577c551554108cd30669
Ilie Cristian Marius <Ilie_Cristian_Marius@hotmail.com>
2021-04-15 00:22:18 +0300

    Added and extended Identity.

---
+++ CriliesRentalPlaceWeb/Areas/Identity/Data/CriliesRentalPlaceWebContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CriliesRentalPlaceWeb.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CriliesRentalPlaceWeb.Data
+{
+    public class CriliesRentalPlaceWebContext : IdentityDbContext<ApplicationUser>
+    {
+        public CriliesRentalPlaceWebContext(DbContextOptions<CriliesRentalPlaceWebContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ CriliesRentalPlaceWeb/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CriliesRentalPlaceWeb.Areas.Identity.Data;
+using CriliesRentalPlaceWeb.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CriliesRentalPlaceWeb.Areas.Identity.IdentityHostingStartup))]
+namespace CriliesRentalPlaceWeb.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<CriliesRentalPlaceWebContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("CriliesRentalPlaceWebContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<CriliesRentalPlaceWebContext>();
+            });
+        }
+    }
+}
