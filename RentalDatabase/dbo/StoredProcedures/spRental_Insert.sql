0cecabf828fd4e97282f31231fd6d88c23d8a9b4
Ilie Cristian Marius <Ilie_Cristian_Marius@hotmail.com>
2021-06-12 22:15:05 +0300

    Updated Rental_Insert stored procedure, to include the rentalStatusId

--- RentalDatabase/dbo/StoredProcedures/spRental_Insert.sql
+++ RentalDatabase/dbo/StoredProcedures/spRental_Insert.sql
@@ -4,12 +4,13 @@
 	@rentedQuantity int,
 	@startDate datetime2(7),
 	@endDate datetime2(7),
-	@totalPrice money
+	@totalPrice money,
+	@rentalStatusId int
 AS
 BEGIN
 	SET NOCOUNT ON
 
-	INSERT INTO dbo.Rental(CustomerId,ProductId,RentedQuantity,StartDate,EndDate,TotalPrice)
-	VALUES (@customerId, @productId, @rentedQuantity, @startDate, @endDate, @totalPrice);
+	INSERT INTO dbo.Rental(CustomerId,ProductId,RentedQuantity,StartDate,EndDate,TotalPrice, RentalStatusId)
+	VALUES (@customerId, @productId, @rentedQuantity, @startDate, @endDate, @totalPrice, @rentalStatusId);
 	SELECT CAST(SCOPE_IDENTITY() as int);
 END
